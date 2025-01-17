017a0e8b21d06b7b56d4c4015187808ece75383a
Mangal-Aditya <mangaladityachatterjee2@gmail.com>
2025-01-17 20:27:24 +0530

    Pushing

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args){
         System.out.println("Hello World");
-        System.out.println("This is the project that I am going to share to the gitHub and form now i have multiple language that I upload  in github");
+        System.out.println("This is the project that I am going to share to the gitHub and from now I have multiple language that I upload  in github");
 
         int a = 10;
         int b = 20 ;
