7b5a2c48fb54a506e756753b18f9a3227395b881
Mangal-Aditya <mangaladityachatterjee2@gmail.com>
2025-01-17 20:26:22 +0530

    Pushing

--- Main.java
+++ Main.java
@@ -1,6 +1,11 @@
 public class Main {
     public static void main(String[] args){
         System.out.println("Hello World");
-        
+        System.out.println("This is the project that I am going to share to the gitHub and form now i have multiple language that I upload  in github");
+
+        int a = 10;
+        int b = 20 ;
+        int sum = a+b;
+         System.out.println("The sum is " + sum);
     }
 }
